388119a976964fb095f2f9f46405f93d09cf0d17
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2022-05-29 22:51:23 -0400

    added Player object

--- Resources/pypoker/game/game_objects.py
+++ Resources/pypoker/game/game_objects.py
@@ -1,5 +1,35 @@
-from game_items.default_objects import Deck, Board
+from pypoker.game_items.default_objects import Card, Deck, Board
 
 class Player:
     def __init__(self) -> None:
-        pass
+        self.__hand = (None, None)
+        self.__chips = 0
+        self.button = None
+        
+    def set_chips(self, chips: float) -> None:
+        self.__chips = chips
+        
+    def set_hand(self, hand: tuple[Card] = None) -> None:
+        if hand is None:
+            self.__hand = (None, None)
+        else:
+            self.__hand = hand
+            
+    def set_button(self, button: str) -> None:
+        # Create class for button?
+        self.button = button
+    
+    def add_chips(self, chips: float) -> None:
+        self.__chips += chips
+
+    def place_bet(self, chips: float) -> None:
+        self.__chips -= chips
+    
+    def has_chips(self) -> bool:
+        return self.__chips > 0
+    
+    def __str__(self) -> str:
+        hand = self.__hand
+        rep = f'{self.__chips},({hand[0]};{hand[1]})'
+        btn_rep = f'{rep},{self.button}'
+        return btn_rep if self.button else rep
--- Resources/pypoker/tests/test_game_objects.py
+++ Resources/pypoker/tests/test_game_objects.py
@@ -1,1 +1,16 @@
-from pypoker.game.game_objects import *
+from pypoker.game.game_objects import Player
+from pypoker.game_items.default_objects import Deck
+deck = Deck()
+
+test_player = Player()
+test_player.set_chips(69.00)
+test_player.set_hand(tuple(deck.r_draw_cards(2)))
+
+def test_player_obj():
+    print(str(test_player))
+    test_player.place_bet(69.00)
+    assert test_player.has_chips() == False
+    
+    test_player.set_button('BB')
+    print(test_player)
+    assert len(str(test_player).split(',')) == 3
